40cf2bebbab5dac2cec88df4331a545668b32fc6
Michael Chen <michael.chen@ichoosr.com>
2021-03-29 18:02:28 +0900

    メニュープラグイン追加

---
+++ kzmz_MenuStatus.js
@@ -0,0 +1,359 @@
+/*:ja
+ * @plugindesc メニューステータスの自由設置 - v1.01
+ * @author 剣崎宗二
+ *
+ * @param Status Rows
+ * @desc キャラを縦に並べる数。
+ * @type number
+ * @min 1
+ * @default 4
+ *
+ * @param Status Cols
+ * @desc キャラを横に並べる数。
+ * @type number
+ * @min 1
+ * @default 1
+ * 
+ * @param Status Window X
+ * @desc メニューステータスのX（-1の場合はデフォルト動作)。
+ * @type number
+ * @min -1
+ * @default -1
+ *
+ * @param Status Window Y
+ * @desc メニューステータスのY（-1の場合はデフォルト動作)。
+ * @type number
+ * @min -1
+ * @default -1
+ * 
+ * @param Status Window Width
+ * @desc メニューステータスの横幅（-1の場合はデフォルト動作)。
+ * @type number
+ * @min -1
+ * @default 300
+ *
+ * @param Status Window Height
+ * @desc メニューステータスの縦幅（-1の場合はデフォルト動作)。
+ * @type number
+ * @min -1
+ * @default 600
+ * 
+ * @param Status Window Padding
+ * @desc メニューウィンドウの外周白地のサイズ。
+ * @type number
+ * @min 0
+ * @default 18
+ * 
+ * @param State X
+ * @desc ステートアイコンエリアの相対座標のX。
+ * @type number
+ * @default 0
+ * 
+ * @param State Y
+ * @desc ステートアイコンエリアの相対座標のY。
+ * @type number
+ * @default 0
+ * 
+ * @param State Width
+ * @desc ステートアイコンエリアの横幅。マイナス値の場合アイコンは表示されない。
+ * @type number
+ * @min -1
+ * @default 144
+ * 
+ * @param Actor Icon X
+ * @desc アクターアイコンのX。Yとどちらかがマイナスだった場合、表示されない。
+ * @type number
+ * @min -1
+ * @default -1
+ * 
+ * @param Actor Icon Y
+ * @desc アクターアイコンのY。Xとどちらかがマイナスだった場合、表示されない
+ * @type number
+ * @min -1
+ * @default -1
+ * 
+ * @param Background PNG
+ * @desc メニューウィンドウの背景ファイル。Systemフォルダ内。この欄が空白でデフォルト状態。
+ * @type string
+ * @default 
+ * 
+ * @param Display Text
+ * @desc 表示するテキスト。,区切りで 内容,x,y,横幅,文字サイズ,文字色ID,アウトライン色ID,フォント名 。値がなければデフォルト。
+ * @type string[]
+ * @default ["a.name(),10,10,30","a.mhp,40,10,30,38,10,,10"]
+ * 
+ * @param Display Picture
+ * @desc 表示するピクチャ、複数指定可でpicturesフォルダ内。,区切りで ピクチャ名,x,y 。
+ * @type string[]
+ * @default ["a.actor().meta.stand_picture,10,10,30"]
+ * 
+ * @param Display Gauge
+ * @desc 表示するゲージ。,区切りで 変動値,最大値,x,y,ゲージ幅,実体色ID,背景色ID。
+ * @type string[]
+ * @default ["a.mp,a.mmp,10,40,70,20,21"]
+ *
+ * @help kz_MenuStatus.js
+ * ■使用する画像は全て任意の名前が使えます。
+ *　 各パラメータにある配置箇所に注意して下さい。
+ * ■表示テキスト、及びゲージの現在値/最大値には a.hp や a.atk などの他、
+ *　 変数やメモの値といった様々なデータが記載できます。 
+ * ■Display TextとDisplay GaugeにあるIDとはwindow.pngのカラーパレット
+ *　 インデックスです。変更する場合は0から数えて記載して下さい。　
+ * ■テキストに制御文字は使えませんので、アイコンなどを表示したい場合
+ *　 画像としてDisplay Pictureに指定して下さい。
+ *
+ * ■フォントを変えた場合、一度の読み込みでは表示されない可能性があります。
+ *　 他のフォントロード系プラグインなどでカバーする事を推奨します。
+ * 
+ * ■読み取るメモについて（●.meta.○）
+ *　 ●の箇所で場所を指定　a.actor()　　　  ＝アクター
+ * 　 　　　　　　　　　　　a.currentClass() ＝クラス　　　など 
+ *　 ○は値の名前なので分かりやすい名前にしてください。 　 
+ *
+ *
+ * 更新履歴
+ * v1.01 - 事前ロードを行うように改変
+ */
+
+(() => {
+    const script = "kzmz_MenuStatus";
+    const parameters = PluginManager.parameters(script);
+
+    const _rows = Number(parameters['Status Rows'] || 4);
+    const _cols = Number(parameters['Status Cols'] || 1);
+    const _wX = Number(parameters['Status Window X'] || -1);
+    const _wY = Number(parameters['Status Window Y'] || -1);
+    const _wWidth = Number(parameters['Status Window Width'] || -1);
+    const _wHeight = Number(parameters['Status Window Height'] || -1);
+    const _wPadding = Number(parameters['Status Window Padding'] || 0);
+    const _wBackground = String(parameters['Background PNG'] || '');
+
+    const _faceX = Number(parameters['Actor Icon X'] || -1);
+    const _faceY = Number(parameters['Actor Icon Y'] || -1);
+
+    const _stateX = Number(parameters['State X'] || 0);
+    const _stateY = Number(parameters['State Y'] || 0);
+    const _stateWidth = Number(parameters['State Width'] || 144);
+
+    const _dTextArray = eval(parameters['Display Text']) || [];
+    const _dPicArray = eval(parameters['Display Picture']) || [];
+    const _dGaugeArray = eval(parameters['Display Gauge']) || [];
+
+    const kz_Scene_Menu_prototype_create = Scene_Menu.prototype.create;
+    Scene_Menu.prototype.create = function () {
+        kz_Scene_Menu_prototype_create.call(this);
+        _dPicArray.forEach(function (line) {
+            $gameParty.members().forEach(function (a) {
+                const dataArray = line.split(',');
+                const value = eval(dataArray[0]);
+                ImageManager.loadPicture(value);
+            }, this);
+        }, this);
+    };
+
+    const kz_Window_MenuStatus_prototype_initialize = Window_MenuStatus.prototype.initialize;
+    Window_MenuStatus.prototype.initialize = function (rect) {
+        rect.x = _wX >= 0 ? _wX : rect.x;
+        rect.y = _wY >= 0 ? _wY : rect.y;
+        rect.width = _wWidth >= 0 ? _wWidth : rect.width;
+        rect.height = _wHeight >= 0 ? _wHeight : rect.height;
+        kz_Window_MenuStatus_prototype_initialize.call(this, rect);
+        if (_wBackground != '') {
+            this.setBackgroundType(2);
+            this.createBackSprite();
+        }
+    };
+
+    Window_MenuStatus.prototype.createBackSprite = function () {
+        this._backSprite = new Sprite();
+        this._backSprite.bitmap = ImageManager.loadSystem(_wBackground);
+        this.addChildToBack(this._backSprite);
+    };
+
+    Window_MenuStatus.prototype.windowWidth = function () {
+        return _wWidth >= 0 ? _wWidth : Graphics.boxWidth - 240;
+    };
+
+    Window_MenuStatus.prototype.windowHeight = function () {
+        return _wHeight >= 0 ? _wHeight : Graphics.boxHeight;
+    };
+
+    Window_MenuStatus.prototype.maxPageRows = function () {
+        return _rows;
+    };
+
+    Window_MenuStatus.prototype.maxCols = function () {
+        return _cols;
+    };
+
+    Window_MenuStatus.prototype.itemHeight = function () {
+        return this.innerHeight / this.maxPageRows();
+    };
+
+    Window_MenuStatus.prototype.drawItem = function (index) {
+        const a = $gameParty.members()[index];
+        if (!a) return;
+
+        this.drawStatusPicture(index);
+        if (_faceX >= 0 && _faceY >= 0) {
+            const rect = this.itemRect(index);
+            this.drawActorFace(a, rect.x + _faceX, rect.y + _faceY, Window_Base._faceWidth, Window_Base._faceHeight);
+        }
+        this.drawStatusGauge(index);
+        this.drawStatusState(index);
+        this.drawStatusText(index);
+    };
+
+    Window_MenuStatus.prototype.drawStatusText = function (index) {
+        const a = $gameParty.members()[index];
+        const rect = this.itemRect(index);
+        _dTextArray.forEach(function (line) {
+            const dataArray = line.split(',');
+            const value = eval(dataArray[0]);
+            const x = Number(dataArray[1]) + rect.x;
+            const y = Number(dataArray[2]) + rect.y;
+            const width = Math.min(rect.width - Number(dataArray[1]), Number(dataArray[3]));
+            const fontSize = Number(dataArray[4]);
+            const textColor = Number(dataArray[5]);
+            const outlineColor = Number(dataArray[6]);
+            const fontFace = dataArray[7];
+
+
+            const currentOutlineColor = this.contents.outlineColor;
+            if (fontFace) { this.contents.fontFace = fontFace; }
+            if (textColor >= 0) { this.changeTextColor(ColorManager.textColor(textColor)); }
+            if (outlineColor >= 0) { this.contents.outlineColor = ColorManager.textColor(outlineColor); }
+            if (fontSize >= 0) { this.contents.fontSize = fontSize; }
+
+
+            this.drawText(value, x, y, width);
+
+            this.resetFontSettings();
+            this.contents.outlineColor = currentOutlineColor;
+        }, this);
+    };
+
+    Window_MenuStatus.prototype.drawStatusPicture = function (index) {
+        const a = $gameParty.members()[index];
+        const rect = this.itemRect(index);
+
+        _dPicArray.forEach(function (line) {
+            const dataArray = line.split(',');
+            const value = eval(dataArray[0]);
+            const bitmap = ImageManager.loadPicture(value);
+            const x = Number(dataArray[1]) + rect.x;
+            const y = Number(dataArray[2]) + rect.y;
+            const pw = bitmap.width;
+            const ph = bitmap.height;
+
+            this.contents.blt(bitmap, 0, 0, pw, ph, x, y);
+        }, this);
+    };
+
+    Window_MenuStatus.prototype.drawStatusGauge = function (index) {
+        const a = $gameParty.members()[index];
+        const rect = this.itemRect(index);
+        _dGaugeArray.forEach(function (line, index) {
+
+            const dataArray = line.split(',');
+            const minValue = Number(eval(dataArray[0]));
+            const maxValue = Number(eval(dataArray[1]));
+            const x = Number(dataArray[2]) + rect.x;
+            const y = Number(dataArray[3]) + rect.y;
+            const width = Math.min(rect.width - Number(dataArray[2]), Number(dataArray[4]));
+            const color1 = ColorManager.textColor(Number(dataArray[5]));
+            const color2 = ColorManager.textColor(Number(dataArray[6]));
+            this.placeExGauge(a, index, x, y, width, minValue, maxValue, color1, color2);
+        }, this);
+    };
+
+    Window_MenuStatus.prototype.drawStatusState = function (index) {
+        if (_stateWidth < 0) return;
+
+        const a = $gameParty.members()[index];
+        const rect = this.itemRect(index);
+        const x = _stateX + rect.x;
+        const y = _stateY + rect.y;
+        const width = Math.min(rect.width - _stateX, _stateWidth);
+
+        this.drawActorIcons(a, x, y, width);
+    };
+
+    Window_MenuStatus.prototype.standardPadding = function () {
+        return _wPadding;
+    };
+
+    Window_MenuStatus.prototype.placeExGauge = function(actor, index, x, y, width, currentValue, maxValue, color1, color2) {
+        const key = "actor%1-exgauge-%2".format(actor.actorId(), index);
+        const sprite = this.createInnerSprite(key, Sprite_ExGauge);
+        sprite.setup(actor, width, currentValue, maxValue, color1, color2);
+        sprite.move(x, y);
+        sprite.show();
+    };
+
+
+    function Sprite_ExGauge() {
+        this.initialize(...arguments);
+    }
+
+    Sprite_ExGauge.prototype = Object.create(Sprite_Gauge.prototype);
+    Sprite_ExGauge.prototype.constructor = Sprite_ExGauge;
+
+    Sprite_ExGauge.prototype.setup = function(battler, width, currentValue, maxValue, color1, color2) {
+        this._battler = battler;
+        this._internalWidth = width;
+        this._currentValueEquation = currentValue;
+        this._maxValueEquation = maxValue;
+        this._valueColor = color1;
+        this._maxColor = color2;
+    };
+
+    Sprite_ExGauge.prototype.bitmapWidth = function() {
+        return this._internalWidth;
+    };
+
+    Sprite_ExGauge.prototype.gaugeX = function () {
+        return 0;
+    };
+
+    Sprite_ExGauge.prototype.smoothness = function() {
+        return 5;
+    };
+
+    Sprite_ExGauge.prototype.isValid = function() {
+        return true;
+    };
+
+    Sprite_ExGauge.prototype.updateFlashing = function() {
+    };
+
+    Sprite_ExGauge.prototype.currentValue = function() {
+        const a = this._battler;
+        return this._currentValueEquation ? eval(this._currentValueEquation) : NaN;
+    };
+
+    Sprite_ExGauge.prototype.currentMaxValue = function() {
+        const a = this._battler;
+        return this._maxValueEquation ? eval(this._maxValueEquation) : NaN;
+    };
+
+    Sprite_ExGauge.prototype.gaugeBackColor = function() {
+        return this._maxColor;
+    };
+
+    Sprite_ExGauge.prototype.gaugeColor1 = function() {
+        return this._valueColor;
+    };
+    
+    Sprite_ExGauge.prototype.gaugeColor2 = function() {
+        return this._valueColor;
+    };
+
+    Sprite_ExGauge.prototype.redraw = function() {
+        this.bitmap.clear();
+        const currentValue = this.currentValue();
+        if (!isNaN(currentValue)) {
+            this.drawGauge();
+        }
+    };
+})();
---
+++ kzmz_PicMenu.js
@@ -0,0 +1,409 @@
+/*:ja
+ * @plugindesc メニューコマンドの画像化（動き有） - v1.02
+ * @author 剣崎宗二
+ *
+ * @param icon width
+ * @desc 画像アイコンの横幅
+ * @type number
+ * @min 0
+ * @default 100
+ *
+ * @param icon height
+ * @desc 画像アイコンの一つ辺りの縦幅
+ * @type number
+ * @min 0
+ * @default 100
+ *
+ * @param icon itemrect
+ * @desc アイコンの間隔
+ * @type number
+ * @min 0
+ * @default 10
+ *
+ * @param icon cols
+ * @desc アイコンを横に並べる数
+ * @type number
+ * @min 1
+ * @default 4 
+ *
+ * @param icon maxexpansion
+ * @desc アイコンの拡縮率
+ * @type number
+ * @min 0
+ * @decimals 2
+ * @default 0.1
+ *
+ * @param x overhead
+ * @desc アイコンを並べる開始位置x
+ * @type number
+ * @min 0
+ * @default 0
+ *
+ * @param y overhead
+ * @desc アイコンを並べる開始位置y
+ * @type number
+ * @min 0
+ * @default 0
+ *
+ * @param commandwindow x
+ * @desc コマンドウインドウの位置ｘ
+ * @type number
+ * @min 0
+ * @default 0
+ *
+ * @param commandwindow y
+ * @desc コマンドウインドウの位置y
+ * @type number
+ * @min 0
+ * @default 0
+ *
+ * @param commandwindow width
+ * @desc コマンドウインドウの幅
+ * @type number
+ * @min 0
+ * @default 240
+ *
+ * @param commandwindow height
+ * @desc コマンドウインドウの高さ
+ * @type number
+ * @min 0
+ * @default 500
+ * 
+ * @param display cursor
+ * @desc カーソルを表示/消去
+ * @type boolean
+ * @on YES
+ * @off NO
+ * @default true
+ *
+ * @param window background file
+ * @desc メニューウィンドウの背景ファイル。空白でデフォルト仕様になります。
+ * @type string
+ * @default 
+ * 
+ * @param Symbol Chart
+ * @desc シンボルと画像の対照表
+ * @type struct<SymbolChart>[]
+ * @default []
+ * 
+ * @param Item Data
+ * @desc 各項目のアニメーションデータ（並び順はメニューと同じ）
+ * @type struct<MenuItemData>[]
+ * @default []
+ *
+ * @help kz_PicMenuEx.js
+ * 
+ * 使用する画像は全て img/system　フォルダ内に入れてください。
+ *
+ * □アイコン用画像…  symbol毎に用意下さい。サイズ、名前は任意です。
+ *　　　　　　　　　　但しサイズがicon width/ icon heightで設定された
+ *　　　　　　　　　　数値以上、以下の場合パラメータの数値サイズに
+ *　　　　　　　　　　自動で縮小/拡大されます。
+ *
+ * □ウインドウ背景用画像…名称任意、パラメータ【window background file】
+ *　 画像を使用しないがウインドウを透過したい場合は1 × 1 pixelの
+ *　 透過画像を用意してその名前をパラメータに入れて下さい。
+ *
+ * □本体ver1.5.2以上の環境を想定しております、それ以下の環境下で
+ *　 ご使用の場合何らかの不具合が出る可能性があります。　
+ *
+ * ■デフォルトsymbol名一覧
+ * 　アイテム　…　item
+ * 　スキル　　…　skill
+ * 　装備　　　…　equip
+ * 　ステータス…　status
+ * 　並び替え　…　formation
+ * 　オプション…　options
+ * 　セーブ　　…　save
+ * 　ゲーム終了…　gameEnd
+ *
+ * 別のプラグインで新しいコマンドを追加した場合
+ * それらのsymbolを入力する事で同様に対応可能です。
+ *
+ * Special Thanks: サイリ(Twitter:sairi55)-アイデア発案、及びサンプル素材制作
+ * 
+ * 
+ * 更新履歴
+ * v1.01 - 1.6.0以前のバージョンで動かない問題を修正
+ * v1.02 - MenuIcon.pngを使用しないにも関わらずないとエラーが出る問題を修正
+ *
+*/
+/*~struct~SymbolChart:
+ * @param symbol
+ * @desc シンボル（item, skills等）
+ * @type string
+ * 
+ * @param pic
+ * @desc ピクチャ名
+ * @type string
+ * 
+*/
+/*~struct~MenuItemData:
+ * 
+ * @param startFrame
+ * @desc 動き始めるフレーム数
+ * @type number
+ * @default 0
+ * 
+ * @param endFrame
+ * @desc 動き終えるフレーム数
+ * @type number
+ * @default 0
+ * 
+ * @param x
+ * @desc 始動時x座標(終着点を0とした場合の相対)
+ * @type number
+ * @min -99999
+ * @default 0
+ * 
+ * @param y
+ * @desc 始動時y座標(終着点を0とした場合の相対)
+ * @type number
+ * @min -99999
+ * @default 0
+ * 
+ * @param alpha
+ * @desc 始動時透明度(終着点は255)
+ * @type number
+ * @default 255
+*/
+
+
+(() => {
+
+    const script = document.currentScript;
+    const parameters = PluginManager.parameters(script);
+
+    const _cmdList = JSON.parse(parameters['Item Data']).map(
+        function (e) {
+            let newObj = JSON.parse(e);
+            return newObj;
+        }
+    ) || [];
+
+    const _symbolList = JSON.parse(parameters['Symbol Chart']).map(
+        function (e) {
+            let newObj = JSON.parse(e);
+            return newObj;
+        }
+    ) || [];
+
+    function findwithSameSymbol(array, element) {
+        if (!array || !element) return undefined;
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].symbol == element.symbol) return array[i];
+        }
+
+        return undefined;
+    }
+
+    const _MenuIconWidth = Number(parameters['icon width'] || 100);
+    const _MenuIconHeight = Number(parameters['icon height'] || 100);
+    const _IconitemRect = Number(parameters['icon itemrect'] || 10);
+    const _iconcols = Number(parameters['icon cols'] || 4);
+    const _MaxExpansion = Number(parameters['icon maxexpansion'] || 0.1);
+    const _xOverhead = Number(parameters['x overhead'] || 0);
+    const _yOverhead = Number(parameters['y overhead'] || 0);
+
+    const _CwindowX = Number(parameters['commandwindow x'] || 0);
+    const _CwindowY = Number(parameters['commandwindow y'] || 0);
+    const _CwindowWidth = Number(parameters['commandwindow width'] || 240);
+    const _CwindowHeight = Number(parameters['commandwindow height'] || 500);
+
+    const _cursorDisplay = eval(parameters['display cursor']);
+
+    const _windowBack = parameters['window background file'] || '';
+
+
+    const kz_Window_MenuCommand_prototype_initialize = Window_MenuCommand.prototype.initialize;
+    Window_MenuCommand.prototype.initialize = function (x, y) {
+        this._commandSprites = [];
+        kz_Window_MenuCommand_prototype_initialize.call(this, _CwindowX, _CwindowY);
+        if (_windowBack != '') {
+            this.setBackgroundType(2);
+            this.createBackSprite();
+        }
+        this.makeCommandSprites();
+        this.select(0);
+    };
+
+    Window_MenuCommand.prototype.createBackSprite = function () {
+        this._backSprite = new Sprite();
+        this._backSprite.bitmap = ImageManager.loadSystem(_windowBack);
+        this.addChildToBack(this._backSprite);
+    };
+
+    Window_MenuCommand.prototype.drawItem = function (index) {
+    };
+
+    Window_MenuCommand.prototype.makeCommandSprites = function () {
+        this._commandSprites = [];
+        this.clearCommandList();
+        this.makeCommandList();
+        this._list.forEach(function (element, i) {
+            const symbolData = findwithSameSymbol(_symbolList, element);
+
+            if (!symbolData) {
+                console.log(element);
+                console.log(_symbolList);
+            }
+
+            let sprite = new Sprite_MenuCommand(_cmdList[i], this.itemRect(i));
+            sprite.bitmap = ImageManager.loadSystem(symbolData.pic);
+            this._commandSprites.push(sprite);
+            this.addChild(sprite);
+        }, this);
+    };
+
+    Window_MenuCommand.prototype.itemWidth = function () {
+        return _MenuIconWidth;
+    };
+
+    Window_MenuCommand.prototype.itemHeight = function () {
+        return _MenuIconHeight;
+    };
+
+
+    const kz_Window_MenuCommand_prototype_itemRect = Window_MenuCommand.prototype.itemRect;
+    Window_MenuCommand.prototype.itemRect = function (index) {
+        const rect = kz_Window_MenuCommand_prototype_itemRect.call(this, index);
+        const maxCols = this.maxCols();
+        rect.x += _xOverhead + (index % maxCol) * _IconitemRect;
+        rect.y = Math.floor(index / maxCols) * (rect.height + _IconitemRect) - this._scrollY + _yOverhead;
+        return rect;
+    };
+
+    Window_MenuCommand.prototype.maxCols = function () {
+        return _iconcols;  //横列数
+    };
+
+    Window_MenuCommand.prototype.windowWidth = function () {
+        return _CwindowWidth;   //横幅
+    };
+
+    Window_MenuCommand.prototype.windowHeight = function () {
+        return _CwindowHeight;   //高さ
+    };
+
+    const kz_Window_MenuCommand_prototype_select = Window_MenuCommand.prototype.select;
+    Window_MenuCommand.prototype.select = function (index) {
+        kz_Window_MenuCommand_prototype_select.call(this, index);
+        this._commandSprites.forEach(function (element) {
+            element.isSelected = false;
+        }, this)
+
+        if (index >= 0 && index < this._commandSprites.length) {
+            this._commandSprites[index].isSelected = true;
+        }
+    };
+
+    const kz_Window_MenuCommand_prototype_ensureCursorVisible = Window_MenuCommand.prototype.ensureCursorVisible
+    Window_MenuCommand.prototype.ensureCursorVisible = function (smooth) {
+        if (!_cursorDisplay) {
+            return false;
+        }
+        return kz_Window_MenuCommand_prototype_ensureCursorVisible.call(this, smooth);
+    };
+
+    const kz_Window_MenuCommand_prototype_processCursorMove = Window_MenuCommand.prototype.processCursorMove;
+    Window_MenuCommand.prototype.processCursorMove = function () {
+        if (!this.isControlAllowed()) return;
+        kz_Window_MenuCommand_prototype_processCursorMove.call(this);
+    };
+
+    const kz_Window_MenuCommand_prototype_processHandling = Window_MenuCommand.prototype.processHandling;
+    Window_MenuCommand.prototype.processHandling = function () {
+        if (!this.isControlAllowed()) return;
+        kz_Window_MenuCommand_prototype_processHandling.call(this);
+    };
+
+    Window_MenuCommand.prototype.isControlAllowed = function () {
+        return !this._commandSprites.some(function (e) {
+            return e.animeFrameCount >= 0;
+        }, this)
+    };
+
+
+
+    function Sprite_MenuCommand() {
+        this.initialize.apply(this, arguments);
+    }
+
+    Sprite_MenuCommand.prototype = Object.create(Sprite.prototype);
+    Sprite_MenuCommand.prototype.constructor = Sprite_MenuCommand;
+
+    Sprite_MenuCommand.prototype.initialize = function (cmdData, baseRect) {
+        Sprite_MenuCommand.prototype.initialize.call(this);
+        this.baseRect = baseRect;
+        this.cmdData = cmdData;
+        this.animeFrameCount = 0;
+        this.expansionCount = 0;
+
+        this.x = Number(this.cmdData.x) + this.baseRect.x + Number(this.baseRect.height / 2);
+        this.y = Number(this.cmdData.y) + this.baseRect.y + Number(this.baseRect.height / 2);
+        this.opacity = Number(this.cmdData.alpha);
+        this.anchor.x = 0.5;
+        this.anchor.y = 0.5;
+    };
+
+    Sprite_MenuCommand.prototype.update = function () {
+        Sprite_MenuCommand.prototype.update.call(this);
+        this.updateRectFit();
+        this.updateSelected();
+        this.updateAnimeMove();
+    };
+
+    Sprite_MenuCommand.prototype.updateRectFit = function () {
+        if (!this.bitmap || !this.bitmap.isReady()) return;
+        this.scale.x = _MenuIconWidth / this.bitmap.width;
+        this.scale.y = _MenuIconHeight / this.bitmap.height;
+    }
+
+    Sprite_MenuCommand.prototype.updateSelected = function () {
+        if (!this.parent.isControlAllowed()) return;  //移動中は拡縮なし
+
+        if (!this.isSelected) {
+            this.expansionCount = 0;
+        }
+        else {
+            //選択されてる項目
+            this.expansionCount++;
+        }
+
+        const expScale = 1 + Math.sin((this.expansionCount % 60) / 60 * Math.PI) * _MaxExpansion;
+        this.scale.x *= expScale;
+        this.scale.y *= expScale;
+    }
+
+    Sprite_MenuCommand.prototype.updateAnimeMove = function () {
+        if (!this.baseRect) {
+            this.visible = false;
+            return;
+        }
+
+        const keyX = this.baseRect.x + this.baseRect.width / 2;
+        const keyY = this.baseRect.y + this.baseRect.height / 2;
+
+        if (this.animeFrameCount < 0) return;
+        this.visible = true;
+
+        if (this.animeFrameCount >= Number(this.cmdData.endFrame)) {
+            this.x = keyX;
+            this.y = keyY;
+            this.opacity = 255;
+            this.animeFrameCount = -1;
+            return;
+        }
+
+        if (this.animeFrameCount >= Number(this.cmdData.startFrame)) {
+            const frameLeft = Number(this.cmdData.endFrame) - this.animeFrameCount;
+            this.x += Math.round((keyX - this.x) / frameLeft);
+            this.y += Math.round((keyY - this.y) / frameLeft);
+            this.opacity += Math.round((255 - this.opacity) / frameLeft);
+        }
+
+        this.animeFrameCount++;
+    }
+
+})();
